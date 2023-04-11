2154b1c63fe63a12b8b5179b8fa9cac190568b72
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-04-11 08:31:30 +0530

    Added : Welcome message

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    /**
+     * Method for printing the welcome message.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to basic core programs!");
+    }
+}
