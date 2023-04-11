ba58462644dd8708260d4b2e2446a339b7fdb2ff
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-04-11 10:03:27 +0530

    Added : code for checking year is leap-year or not

--- src/com/basiccoreprogram/BasicPrograms.java
+++ src/com/basiccoreprogram/BasicPrograms.java
@@ -4,43 +4,22 @@ import java.util.Scanner;
 
 public class BasicPrograms {
     /**
-     * Method for flip the coin and count the head and tail occurrence
-     * and printing the head and tail occurrence percentage
-     *
+     * Method for checking the leap-year and printing.
      * @param args
      */
+
     public static void main(String[] args) {
         /*
-         * 1) taking variable and taking the user input for how many times flip the coin
-         * 2) initialize a random variable and check the condition and increase the count
-         * 3) getting the head-percentage and tail-percentage and printing
+         *  Taking the user input and checking the condition
+         *    and printing the year is leap-year or not.
          */
-
-        /*
-        1) taking variable and taking the user input for how many times flip the coin
-         */
-        int headCount = 0, tailCount = 0;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter how many times you flip the coin");
-        int n = scanner.nextInt();
-        for (int i = 1; i <= n; i++) {
-            /*
-             2) initialize a random variable and check the condition and increase the count
-             */
-            double flipCoin = Math.floor(Math.random() * 2);
-            if (flipCoin < 0.5) {
-                tailCount++;
-            } else {
-                headCount++;
-            }
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year!");
+        int year = sc.nextInt();
+        if((year % 4 == 0) && (year % 100 != 0)){
+            System.out.println(year + " is a leap-year");
+        }else {
+            System.out.println(year + " is not a leap-year");
         }
-        /*
-        3) getting the head-percentage and tail-percentage and printing
-         */
-        double headPercentage = (double) headCount / n * 100;
-        double tailPercentage = (double) tailCount / n * 100;
-        System.out.println("Head percentage of flip coin is : " + headPercentage + "%");
-        System.out.println("Tail percentage of flip coin is : " + tailPercentage + "%");
     }
 }
